cf14c4a3f2a3edb73a2610e18c0cb9887ac37002
Anhgelus Morhtuuzh <william@herges.fr>
2026-07-29 22:06:04 +0200

    Revert "build(go): use zig cc"
    33cb227523107979384d38b0812a752cb780d679

--- go/build.zig
+++ go/build.zig
@@ -39,24 +39,21 @@ pub fn build(b: *std.Build) !void {
 
 fn buildGo(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, comptime command: []const u8) *std.Build.Step.Run {
     var flags = std.ArrayList(u8).initCapacity(b.allocator, 2) catch unreachable;
-    flags.appendSlice(b.allocator, "-linkmode external") catch unreachable;
-    if (optimize != .Debug) flags.appendSlice(b.allocator, " -s") catch unreachable;
-    const targetStr = std.fmt.allocPrint(b.allocator, "{s}-{s}-{s}", .{
-        @tagName(target.result.cpu.arch),
-        @tagName(target.result.os.tag),
-        @tagName(target.result.abi),
-    }) catch unreachable;
-    const cc = std.fmt.allocPrint(b.allocator, "zig cc -target {s}", .{targetStr}) catch unreachable;
-    const cpp = std.fmt.allocPrint(b.allocator, "zig c++ -target {s}", .{targetStr}) catch unreachable;
+    flags.appendSlice(b.allocator, "-linkmode external -e") catch unreachable;
+    if (optimize != .Debug) flags.appendSlice(b.allocator, "-w -s") catch unreachable;
+    const cc_ldflags = std.fmt.allocPrint(
+        b.allocator,
+        "{s}/libtypdown.a",
+        .{b.lib_dir},
+    ) catch unreachable;
     const run = b.addSystemCommand(&[_][]const u8{
         "go",       command,
         "-ldflags", flags.items,
         ".",
     });
-    run.setEnvironmentVariable("CC", cc);
-    run.setEnvironmentVariable("C++", cpp);
+    run.setName("building go");
     run.setEnvironmentVariable("CGO_ENABLED", "1");
-    run.setEnvironmentVariable("CGO_LDFLAGS", "-L./zig-out/lib -ltypdown ./zig-out/lib/libtypdown.a");
+    run.setEnvironmentVariable("CGO_LDFLAGS", cc_ldflags);
     run.setEnvironmentVariable("GOOS", @tagName(target.result.os.tag));
     return run;
 }
